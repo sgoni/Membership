0c119d74b80a4039129d4db9628268e65ffcb2f7
s.goni <steven.goni@outlook.com>
2025-04-05 22:49:44 -0600

    Implementing docker container deployment.

--- src/backend/src/Services/Member/Person.BackEnd.API/Exceptions/PersonNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Person.BackEnd.API.Exceptions;
-
-public class PersonNotFoundException : NotFoundException
-{
-    public PersonNotFoundException(object key) : base("person", key)
-    {
-    }
-}
